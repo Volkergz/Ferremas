c7eeda11e3d384ab3f47f15bae2584479f3eea42
Angel <an.perugini@duocuc.cl>
2025-05-06 02:20:32 -0400

    home

--- main/frontend/polls/views.py
+++ main/frontend/polls/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'login.html', {
+    return render(request, 'home.html', {
         'title': 'Polls Index',
         'message': 'Welcome to the Polls Index Page!'
+    })
+
+def login(request):
+    return render(request, 'login.html', {
+        'title': 'Login Page',
+        'message': 'Please log in to continue.'
     })
 
 
